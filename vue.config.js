ca6705b1400a98d860c2ae9ef43e050f956c59f6
Tiegist Girma <tigistgirma40@gmail.com>
2025-02-09 20:55:52 +0300

    adding vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/your-repo-name/'
+      ? '/deprepo/'
       : '/',
     configureWebpack: {
       // Add any additional configurations here
