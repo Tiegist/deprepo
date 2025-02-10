ba510163f43f0e7a9a2cdcf1747839ad36497777
Tiegist Girma <tigistgirma40@gmail.com>
2025-02-10 13:54:32 +0300

    updating vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,21 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/deprepo/'
-      : '/',
-    configureWebpack: {
-      // Add any additional configurations here
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
+// Define the Vite configuration
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    // Other options can also be specified here
-  }
+  },
+  // Set the public path based on the environment
+  base: process.env.NODE_ENV === 'production'
+    ? '/deprepo/'
+    : '/',
+});
